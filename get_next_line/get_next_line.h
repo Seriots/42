94fad6d46dfc065ada891c2ff0ce1599aaa9e465
Seriots <96475943+Seriots@users.noreply.github.com>
2022-05-04 14:36:42 +0200

    Add files via upload

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -1,12 +1,15 @@
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
 #include <stdlib.h>
 #include <unistd.h>
 
 /*get_next_line.c*/
 char    *get_next_line(int fd);
-char    *ft_read_recurse(int fd, char *prev_read, char *buf_tampon, int size);
+char	*ft_read_recurse(int fd, char *prev_read, char *buf_tampon, int size);
 char    *ft_getline(char *buffer, char *prev_read, int position, int buf_empty);
 int     ft_checkbuffer(char *buf);
 
